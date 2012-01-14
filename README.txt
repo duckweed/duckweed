9dd86ed607e4a55a7cb854eb73499c4f21a7e828
wategancreek <andrew.oxenburgh@gmail.com>
2012-01-15 12:58:17 +1300

    now does some webdriver tests. ready for actual development

---
+++ README.txt
@@ -0,0 +1,18 @@
+Duckweed Collaboration
+======================
+
+To run:
+
+mvn package gae:run
+
+To package:
+
+mvn package gae:deploy
+
+To run integration tests:
+
+in one window run -
+mvn gae:deploy
+
+and in another run -
+mvn verify
---
+++ src/test/java/org/duckweedcoll/Sum_wb_ITCase.java
@@ -0,0 +1,61 @@
+package org.duckweedcoll;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static org.junit.Assert.assertTrue;
+
+public class Sum_wb_ITCase {
+
+
+    private static WebDriver driver;
+
+    @Test
+    public void test_whenDigitsAreEntered_thenSummedCorrectly() {
+        checkSumming("simple sum", "1", "2", "3");
+        checkSumming("same numbers", "2", "2", "4");
+        checkSumming("pos and neg", "-2", "2", "0");
+        checkSumming("neg and pos", "2", "-2", "0");
+        checkSumming("unneeded prefix zeros", "02", "02", "4");
+        checkSumming("double digits", "17", "19", "36");
+        checkSumming("preceding space in 'A'", " 17", "19", "36");
+        checkSumming("preceding space in 'B'", "17", " 19", "36");
+        checkSumming("preceding space in both", " 17", " 19", "36");
+    }
+
+    @Test
+    public void test_whenNonNumericIsEntered_thenThrowException() {
+        driver.findElement(By.name("a")).sendKeys("non numeric");
+        driver.findElement(By.name("Send")).submit();
+
+        assertSourceContains("should throw a numeric exception", "NumberFormatException");
+    }
+
+
+    private void checkSumming(String message, String valueA, String valueB, String expectedValue) {
+        driver.findElement(By.name("a")).sendKeys(valueA);
+        driver.findElement(By.name("b")).sendKeys(valueB);
+        driver.findElement(By.name("b")).submit();
+
+        assertSourceContains(message, "Sum is " + expectedValue);
+    }
+
+    private void assertSourceContains(String message, String expectedString) {
+        assertTrue(message, driver.getPageSource().contains(expectedString));
+    }
+
+    @AfterClass
+    public static void after() {
+        driver.close();
+    }
+
+    @BeforeClass
+    public static void before() {
+        driver = new FirefoxDriver();
+        driver.get("http://localhost:8080/sum.groovy");
+    }
+}
